0ff691f2fdba9b5c8b73818a9e56e0d79c6da00d
Can Yoldas <canyoldas1997@gmail.com>
2023-12-24 21:07:16 +0300

    wip status code

--- Sources/CYNetwork/APIClient.swift
+++ Sources/CYNetwork/APIClient.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-typealias NetworkResponse = (data: Data, response: URLResponse)
+typealias NetworkResponse = (data: Data, urlResponse: URLResponse)
 
 enum LogLevel {
     case verbose
@@ -19,17 +19,38 @@ final public class APIClient {
     private let logger = AppLogger(category: "BaseAPI")
     
     public func execute<T: Decodable>(request: URLRequest) async throws -> T {
-        
         logger.debug("Execute Called for path: \(request.url?.absoluteString ?? "")")
-        let response: NetworkResponse = try await session.data(for: request)
-        logger.debug("\(String(data: response.data, encoding: .utf8) ?? "")")
+        let networkResponse: NetworkResponse = try await session.data(for: request)
+        
+        if let httpResponse = networkResponse.urlResponse as? HTTPURLResponse {
+            
+            switch httpResponse.statusCode {
+                
+            case 200..<300:
+                return try handleSuccessStatusCode(networkResponse.data)
+            case 300...400:
+                
+            }
+            
+        }
+        
+        
+//        do {
+//            let decodedData = try jsonDecoder.decode(T.self, from: response.data)
+//            logger.debug("Successful: \(request.url?.absoluteString ?? "")")
+//            return decodedData
+//        } catch {
+//            logger.error("Decoding error:  \(request.url?.absoluteString ?? "") \n \(error.localizedDescription)")
+//            throw error
+//        }
+    }
+    
+    private func handleSuccessStatusCode<T: Decodable>(_ data: Data) throws -> T {
         do {
-            let decodedData = try jsonDecoder.decode(T.self, from: response.data)
-            logger.debug("Successful: \(request.url?.absoluteString ?? "")")
+            let decodedData = try jsonDecoder.decode(T.self, from: data)
             return decodedData
         } catch {
-            logger.error("Decoding error:  \(request.url?.absoluteString ?? "") \n \(error.localizedDescription)")
-            throw error
+            throw NetworkError.decodingFailed
         }
     }
 }
