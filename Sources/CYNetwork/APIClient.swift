754017662496228baabd8cbc6c0b729d5580f0a9
Can Yoldas <canyoldas1997@gmail.com>
2023-12-24 19:37:26 +0300

    updated api client

--- Sources/CYNetwork/APIClient.swift
+++ Sources/CYNetwork/APIClient.swift
@@ -1,5 +1,37 @@
 import Foundation
 
-final public class APIClient { }
+typealias NetworkResponse = (data: Data, response: URLResponse)
+
+enum LogLevel {
+    case verbose
+}
+
+final public class APIClient {
+    
+    public init(
+        session: URLSession
+    ) {
+        self.session = session
+    }
+    
+    private let session: URLSession
+    private let jsonDecoder = JSONDecoder()
+    private let logger = AppLogger(category: "BaseAPI")
+    
+    public func execute<T: Decodable>(request: URLRequest) async throws -> T {
+        
+        logger.debug("Execute Called for path: \(request.url?.absoluteString ?? "")")
+        let response: NetworkResponse = try await session.data(for: request)
+        logger.debug("\(String(data: response.data, encoding: .utf8) ?? "")")
+        do {
+            let decodedData = try jsonDecoder.decode(T.self, from: response.data)
+            logger.debug("Successful: \(request.url?.absoluteString ?? "")")
+            return decodedData
+        } catch {
+            logger.error("Decoding error:  \(request.url?.absoluteString ?? "") \n \(error.localizedDescription)")
+            throw error
+        }
+    }
+}
 
 
