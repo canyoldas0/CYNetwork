37bdac24c7abb0f4ecf330eeb49bb36e3f2d7745
Can Yoldas <canyoldas1997@gmail.com>
2023-12-29 00:41:20 +0300

    wip

--- Sources/CYNetwork/RequestEncoding/ParameterEncoding.swift
+++ Sources/CYNetwork/RequestEncoding/ParameterEncoding.swift
@@ -48,11 +48,6 @@ struct URLParameterEncoder: ParameterEncoder {
             }
             urlRequest.url = urlComponents.url
         }
-        
-        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
-            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        }
-        
     }
 }
 
